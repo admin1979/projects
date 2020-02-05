16c1ec98310742bd450529d552d9df5e43c64e10
Oleg <oleg.bachinskiy@gmail.com>
2020-02-05 12:10:23 +0300

    Задание 1

--- JS/taskJS1/script.js
+++ JS/taskJS1/script.js
@@ -1,5 +1,5 @@
-let money = prompt ('Ваш бюджет на месяц?');
-let time = prompt ("Введите дату в формате", "YYYY-MM-DD?");
+let money = prompt ('Ваш бюджет на месяц?', '');
+let time = prompt ("Введите дату в формате", "YYYY-MM-DD?", '');
 console.log(money);
 console.log(time);
 
@@ -11,3 +11,13 @@ let appData = {
     income: [],
     savings: false
 };
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.budget / 30);
