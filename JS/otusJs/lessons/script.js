4d4e703f8c99b675cb1bba691a4cdfe0971e3e90
Oleg <oleg.bachinskiy@gmail.com>
2020-02-08 13:25:16 +0300

    Otus 2.1.1 и 2.1.2

--- JS/otusJs/lessons/script.js
+++ JS/otusJs/lessons/script.js
@@ -7,11 +7,15 @@ console.log('Hello user!');
 alert('Hello world!');
 */
 
-const auto = {
+
+//----------------------------------------------------------
+//Что такое объект
+
+/* const auto = {
     model: "Kalina",
     color: "Red",
     year: 2012
-}
+};
 
 const input = prompt('Enter a property:');
 const value = auto[input];
@@ -21,4 +25,24 @@ if (value) {
 } else {
     console.log('Not found');
 }
+ */
 
+//-------------------------------------------------------------- 
+//Добавление и удаление свойств
+
+const auto = {
+    model: "Kalina",
+    color: "Red",
+    year: 2012
+};
+
+const input = prompt ("Enter a property: ");
+const hasProp = input in auto; //есть или нет такое свойство в объекте
+
+if(hasProp) {
+  delete auto[input];
+} else {
+    auto[input] = null;
+}
+
+console.log(auto);
