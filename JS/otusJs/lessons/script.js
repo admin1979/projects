c8ebaac56b21be1a168e4cd2e4d55d994932a7a1
Oleg <oleg.bachinskiy@gmail.com>
2020-02-10 18:14:12 +0300

    Otus 2.1.4 Деструктуризация объектов. Запись свойств.

--- JS/otusJs/lessons/script.js
+++ JS/otusJs/lessons/script.js
@@ -116,5 +116,131 @@ storeGood.number = 2000;
 
 console.log(storeGood, shopGood);
 
+//-----------------------------------------------------------------------------------
+//2.1.4 Деструктуризация объектов. Запись свойств.
+let block = {
+    width: 170,
+    height: 90
+};
+
+let width = block.width;
+
+console.log(width);
+//-------------------------//
+
+let block = {
+    width: 170,
+    height: 90,
+    price: 290,
+    type: 'Solid'
+};
+
+let width = block.width;
+let height = block.height;
+let price = block.price;
+let type = block.type;
+
+console.log(width, height, price, type);
+
+//------------------------------------------------//
+let block = {
+    width: 170,
+    height: 90,
+    price: 290,
+    type: 'Solid'
+};
+
+let {width, height, price, type} = block;
+
+console.log(width, price);
+
+
+//------------------------------------------------//
+let block = {width: 170, height: 90};
+
+let {width, height:CamelCase1} = block;
+
+console.log(CamelCase1);
+
+//-------------------------------------------//
+let block = {
+    width: 170
+};
+
+let{width, height = 100} = block;
+
+console.log(block);
+console.log(width);
+console.log(height);
+
+//---------------------------------//
+let block = {
+    width: 170,
+    height: 90,
+    price: 290,
+    type: 'Solid'
+};
+
+let  {width, height, ...rest} =block;
+
+let {type, ...rest1} = block;
+
+console.log(rest);
+console.log(rest1);
+
+//--------------------------------------//
+let block = {
+    width: 170, 
+    height: {value:50, 
+        unit: 'cm'}};
+let {height, ...rest2} = block;
+console.log(rest2);
+
+//---------------------------------------//
+
+let block = {width: 170, height: {value:50, unit: 'cm'}};
+let {height} = block;
+console.log(height);
+
+//--------------------------------------------------//
+let block = {
+    width: 170, 
+    height: {value:50, 
+        unit: 'cm'}
+    };
+
+let {width, height: {value, unit}} = block;
+
+console.log(unit, value);
+
+//-------------------------------------------------//
+let example = {
+    width: 10,
+    height: 15,
+    length: 25,
+    beauty: {chest: 90, 
+            waist: 60, 
+            hips: 90}
+};
+
+let {width, height, length, beauty: {chest, waist, hips}} = example;
+
+console.log(chest+waist+height);
+
+//----------------------------------------------------//
+let person = {
+    name: 'Sam',
+    years: 25
+};
+
+let {name, years:age, height = null} = person;
+
+console.log(name, age, height);
+
+
+
+
+
+
 
 
