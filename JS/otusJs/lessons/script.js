23c7f68bd364402788fdf90d5265ba9e62c86de2
Oleg <oleg.bachinskiy@gmail.com>
2020-02-10 14:13:12 +0300

    Otus 2.1.3 Копирование объектов

--- JS/otusJs/lessons/script.js
+++ JS/otusJs/lessons/script.js
@@ -30,7 +30,7 @@ if (value) {
 //-------------------------------------------------------------- 
 //Добавление и удаление свойств
 
-const auto = {
+/* const auto = {
     model: "Kalina",
     color: "Red",
     year: 2012
@@ -45,4 +45,76 @@ if(hasProp) {
     auto[input] = null;
 }
 
-console.log(auto);
+console.log(auto); */
+
+//--------------------------------------------------------------------------
+//Копирование объектов / Ссылка на объект
+
+let value = {
+    width: 150,
+    color: 'green'
+};
+
+let newValue = value;
+
+value.width = 170;
+
+console.log(newValue);
+console.log(value);
+
+//-------------------//
+const block = {};
+block.width = 140;
+block.color = 'blue';
+
+console.log(block);
+
+//------------------//
+let obj = {
+    width: 190,
+    height: 350,
+    color: 'yellow',
+    age: 15
+};
+
+let clone = Object.assign ({},obj);  
+
+console.log(clone);
+//------------------//
+
+let obj = {width: 120, color: 'grey', speed: '25 km/h'};
+
+Object.assign (obj,{width: 900, height: 500, speed: '100 km/h'});
+
+console.log(obj);
+//-----------------//
+
+let obj = {width: 180};
+
+Object.assign (obj, {width: 600},{height: 100});
+
+console.log(obj);
+//-----------------//
+
+let product = {numberProduct: 133, storageSpace: 99, weight: '94 kg'};
+
+let item = Object.assign (product,{price: "100 руб"});
+
+console.log(item);
+
+//-----------------//
+
+const storeGood = {
+    number: 1001,
+    place: 'A101',
+    weight: 25
+};
+
+const shopGood = Object.assign({}, storeGood, {cost: 10});
+
+storeGood.number = 2000;
+
+console.log(storeGood, shopGood);
+
+
+
