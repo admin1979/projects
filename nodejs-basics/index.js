020199c51fbbf2bb89e843b840395cf1ca824d6e
Oleg <oleg.bachinskiy@gmail.com>
2020-02-29 17:40:33 +0300

    Добавил папку nodejs-basics это начало курса

---
+++ nodejs-basics/index.js
@@ -0,0 +1,7 @@
+//console.log('Hello!, __dirname');
+//console.log('Hello!, __filename');
+const obj = require('./user');
+
+console.log(obj.user);
+
+obj.sayHello();
---
+++ nodejs-basics/user.js
@@ -0,0 +1,12 @@
+const user = {
+    name: 'Elena',
+    age: 25,
+    minya: 5
+}
+
+module.exports = {
+    user: user,
+    sayHello() {
+        console.log('Hello')
+    }
+}
