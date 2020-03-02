e9e23a6505d8b301908f66328d471acb3e50c1e4
Oleg <oleg.bachinskiy@gmail.com>
2020-03-02 16:07:36 +0300

    Блок 2. Основы Node.js дошел до Модуль OS

--- nodejs-basics/refs/fs_ref.js
+++ nodejs-basics/refs/fs_ref.js
@@ -2,8 +2,47 @@ const fs = require('fs');
 const path = require('path');
 
 // File System
-fs.mkdir(path.join(__dirname, 'notes'),err => {
-    if (err) throw new Error(err)
+// fs.mkdir(path.join(__dirname, 'notes'),err => {
+//     if (err) throw new Error(err)
 
-    console.log('Папка было создана')
-});
+//     console.log('Папка было создана')
+// });
+
+// fs.writeFile(
+//     path.join (__dirname, 'notes', 'mynotes.txt'),
+//     'Hello World!',
+//     (err) => {
+//         if (err) throw err
+//         console.log("Файл был создан");
+
+//         fs.appendFile(path.join (__dirname, 'notes', 'mynotes.txt'),
+//             ' From append file',
+//             err => {
+//                 if (err) throw err
+//                 console.log("Файл был изменён");
+
+//                 fs.readFile(
+//                     path.join (__dirname, 'notes', 'mynotes.txt'),
+//                     'utf-8',
+//                     (err, data) => {
+//                         if(err) throw err
+//                         console.log(data);
+//                     }
+//                 );
+
+//                 }
+//             );
+//         }
+//     );
+
+fs.rename(
+    path.join(__dirname, 'notes', 'mynotes.txt'),
+    path.join(__dirname, 'notes', 'notes.txt'),
+    err => {
+        if (err) throw err
+
+        console.log('Файл переименован');
+    }
+);
+
+  
