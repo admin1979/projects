f7164c919a5a7eecfa80016c8dccfd1fe3aba3af
Oleg <oleg.bachinskiy@gmail.com>
2020-03-10 17:38:19 +0300

    Блок 3. Практика: Express.js/ Дошел до Создание модели

--- node-express/index.js
+++ node-express/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars')
+const homeRoutes = require('./routes/home')
+const addRoutes = require('./routes/add')
+const coursesRoutes = require('./routes/courses')
+
 
 
 const app = express();
@@ -15,22 +19,10 @@ app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 app.use(express.static('public'));
-
-
-
-app.get('/add', (req, res) => {
-    res.render('add', {
-        title: 'Добавить курс',
-        isAdd: true
-    })
-});
-
-app.get('/courses', (req, res) => {
-    res.render('courses',{
-       title: 'Курсы',
-       isCourses: true
-    })
-});
+app.use(express.urlencoded({extended: true}))
+app.use('/', homeRoutes)
+app.use('/add', addRoutes)
+app.use('/courses', coursesRoutes)
 
 
 const PORT = process.env.PORT || 3000;
--- node-express/routes/home.js
+++ node-express/routes/home.js
@@ -1,7 +1,7 @@
 const {Router} = require('express');
 const router = Router()
 
-router.get('/', (req, res){
+router.get('/', (req, res) => {
     res.render('index', {
         title: 'Главная страница',
         isHome: true
