16df6cfb6503115a8c5cd22986248ddc99138b71
Oleg <oleg.bachinskiy@gmail.com>
2020-03-12 12:55:03 +0300

    Node myserver /Закончил папку 03 Маршрутизация

--- myserver/index.js
+++ myserver/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const booksRouter = express.Router();
 
 const app = express(); //its working
 const products = ['Apple', 'Pen', 'Computer'];
@@ -7,13 +8,49 @@ app.get("/", (req, res, next) => {
     res.send("Its working");
 });
 
+
 app.get('/products', (req, res, next) => {
+    console.log('Page', req.query.page);
     res.send(products);
 });
 
-app.get('/products', (req, res, next) => {
-    res.send(1);
+
+app.get('/products/:id', (req, res, next) => {
+    if (products[req.params.id]) {
+        res.send(products[req.params.id]);
+    } else {
+        res.status(403).send('Product not found');
+    }
 });
+
+app.get('/downloadBooks111', (req, res, next) => {
+    res.download('./public/books.html', 'anothername', (err) => {
+        console.log('File sent');
+    });
+});
+
+
+booksRouter.get('/', (req, res) => {
+    res.send('Books11');
+});
+
+booksRouter.get('/about', (req, res) => {
+    res.send('About books');
+});
+
+app.get('/blog', (req, res, next) => {
+    res.redirect('http://big-moto.ru');
+});
+
+app.get('/blog2', (req, res, next) => {
+    res.redirect('/');
+});
+
+
+
+app.use('/books', booksRouter);
+
+
 
 app.listen(5000, () => {
     console.log('Its started', new Date());
