0477c3cde1b7ee0077bf7add9b9514c0c682002e
Oleg <oleg.bachinskiy@gmail.com>
2020-03-17 17:16:03 +0300

    до 007 Строки

--- JS/modernJS 2019/lessonsModernJS/app.js
+++ JS/modernJS 2019/lessonsModernJS/app.js
@@ -1,4 +1,4 @@
-const user = {
+/* const user = {
     firstName: 'Denis',
     age: 30,
     isAdmin: true,
@@ -16,4 +16,56 @@ value = user['isAdmin'];
 
 
 
+console.log(value);
+
+//005 Преобразование типов---------------------------------------------------------------
+let value;
+
+value = (10 + 11 + 'Privet').toString();
+
+value = String({
+    name: 'Denis',
+    age: 16
+});
+
+value = Number(123);
+
+value = parseFloat('12.03as');
+value = parseInt('12.03as');
+
+
+console.log(value);
+console.log(typeof value);
+
+//006 Числа---------------------------------------------------------------------------------
+
+let value;
+
+value = 5 % 2;
+
+value += 100;
+
+value++;
+
+--value;
+
+value = 0.6 + 0.7;
+value = +value.toFixed(1);
+
+value = Math;
+value = Math.PI;
+
+const rrr = [1, 4, 6, 10, 19, "flowerPower"];
+const eee = rrr[Math.floor(Math.random() * rrr.length)];
+
+console.log(eee);
+console.log(typeof eee);
+
+//console.log(value);
+//console.log(typeof value);
+
+//007 Строки----------------------------------------------------------------------------
+
+ */
+const value = 'Olegas' + ' ' + 'Bachiskis';
 console.log(value);
