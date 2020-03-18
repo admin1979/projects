6cb0bbac25733023581a209cc96f2703d5b40c0a
Oleg <oleg.bachinskiy@gmail.com>
2020-03-18 11:57:30 +0300

    07 REST API

--- myserver/index.js
+++ myserver/index.js
@@ -2,90 +2,99 @@ const express = require("express");
 const booksRouter = express.Router();
 
 const app = express(); //its working
-const products = ['Apple', 'Pen', 'Computer'];
+const products = ["Apple", "Pen", "Computer"];
 
 //тут мы работали с pug, выключено
 // app.set('view engine', 'pug');
-app.set('view engine', 'ejs');
-app.set('views', './views');
+//app.set('view engine', 'ejs');
+
+app.set("view engine", "hbs");
+app.set("views", "./views");
 
 app.use((req, res, next) => {
-    console.log('Date', new Date(), 'Method', req.method, 'URL', req.originalUrl, 'IP', req.ip);
-    next();
+  console.log(
+    "Date",
+    new Date(),
+    "Method",
+    req.method,
+    "URL",
+    req.originalUrl,
+    "IP",
+    req.ip
+  );
+  next();
 });
 
-
-
-app.use(express.static('public'));
-
+app.use(express.static("public"));
 
 app.get("/", (req, res, next) => {
-    res.send("Its working");
+  res.send("Its working");
 });
 
-
-app.get('/products', (req, res, next) => {
-    console.log('Page', req.query.page);
-    res.send(products);
+app.get("/products", (req, res, next) => {
+  console.log("Page", req.query.page);
+  res.send(products);
 });
 
-
-app.get('/products/:id', (req, res, next) => {
-    if (products[req.params.id]) {
-        res.send(products[req.params.id]);
-    } else {
-        res.status(403).send('Product not found');
-    }
+app.get("/products/:id", (req, res, next) => {
+  if (products[req.params.id]) {
+    res.send(products[req.params.id]);
+  } else {
+    res.status(403).send("Product not found");
+  }
 });
 
 //pug
-app.get('/main', (req, res, next) => {
-    res.render('main', {
-        title: 'Products',
-        message: 'Products List',
-        products: products
-    });
+app.get("/main", (req, res, next) => {
+  res.render("main", {
+    title: "Products",
+    message: "Products List",
+    products: products
+  });
 });
 
 //ejs
-app.get('/ejs', (req, res, next) => {
-    res.render('main', {
-        title: 'Products1',
-        message: 'Products List1',
-        products: products
-    });
+app.get("/ejs", (req, res, next) => {
+  res.render("main", {
+    title: "Products1",
+    message: "Products List1",
+    products: products
+  });
 });
 
-
-app.get('/downloadBooks111', (req, res, next) => {
-    res.download('./public/books.html', 'anothername', (err) => {
-        console.log('File sent');
-    });
+//hbs
+app.get("/hbs", (req, res, next) => {
+  res.render("main.hbs", {
+    title: "Products1",
+    message: "Products List12",
+    products: products
+  });
 });
 
-
-booksRouter.get('/', (req, res) => {
-    res.send('Books11');
+app.get("/downloadBooks111", (req, res, next) => {
+  res.download("./public/books.html", "anothername", err => {
+    console.log("File sent");
+  });
 });
 
-booksRouter.get('/about', (req, res) => {
-    res.send('About books');
+booksRouter.get("/", (req, res) => {
+  res.send("Books11");
 });
 
-app.get('/blog', (req, res, next) => {
-    res.redirect('http://big-moto.ru');
+booksRouter.get("/about", (req, res) => {
+  res.send("About books");
 });
 
-app.get('/blog2', (req, res, next) => {
-    res.redirect('/');
+app.get("/blog", (req, res, next) => {
+  res.redirect("http://big-moto.ru");
 });
 
+app.get("/blog2", (req, res, next) => {
+  res.redirect("/");
+});
 
-
-app.use('/books', booksRouter);
-
-
+app.use("/books", booksRouter);
 
 app.listen(5000, () => {
-    console.log('Its started', new Date());
+  console.log("Its started", new Date());
 });
