e00a4b7756954161196c788872ee2c4e3feda809
Oleg <oleg.bachinskiy@gmail.com>
2020-03-30 16:13:09 +0300

    commit 84 026 Получение книги по ID - GET__id

--- rest-api/routes/books.js
+++ rest-api/routes/books.js
@@ -1,16 +1,29 @@
 const express = require("express");
 const router = express.Router();
 
-let books = [
-  {
-    id: 1,
-    autor: "Jo Do",
-    title: "JavaScriptBook"
-  }
-];
+let books = [{
+  id: 1,
+  autor: "Jo Do",
+  title: "JavaScriptBook"
+}];
 
 router.get("/", (req, res) => {
   res.json(books);
 });
+
+router.get('/:id', (req, res) => {
+  const bookId = parseInt(req.params.id, 10);
+
+  const book = books.find(book => book.id === bookId);
+
+
+  if (book) {
+    return res.json(book);
+  }
+  return res.status(404).json({
+    status: `Book with ${bookID} Not found`
+  });
+});
+
 
 module.exports = router;
