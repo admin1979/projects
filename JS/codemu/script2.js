329fe731fd2c2d4c93ffbf8faadec129d21e1875
Oleg <oleg.bachinskiy@gmail.com>
2020-04-05 16:23:38 +0300

    Преобразование к логическому типу в JavaScript

--- JS/codemu/script2.js
+++ JS/codemu/script2.js
@@ -50,10 +50,128 @@ let b = 3;
 alert(10 % 3);
 
 alert(2 ** 10);
-*/
 
 let a = 3 * 2 ** (3 + 1);
 alert(a);
 alert(2 ** 5);
 
 //строки
+
+let str = '!!!';
+alert(str);
+
+let j = "java";
+let s = 'script';
+alert(j + s);
+
+let h = 'hello';
+let w = 'world';
+alert(h + ' ' + w);
+
+let name = "Дмитрий Трепачов";
+alert('Пивет ' + name);
+
+let age = '99 лет';
+alert('тебе ' + age + ' ,ура бля!');
+
+let str = 'абвгдейка';
+alert(str.length);
+
+let str1 = `a,
+    b,
+    c`;
+alert(str1);
+
+let x;
+alert(x);
+console.log(x);
+
+let a = null;
+alert(a);
+
+let r = false;
+let y = true;
+// alert(r);
+// alert(y);
+alert(r + y);
+
+let s = 'Dmitro';
+let d = 'Trpachev';
+alert(s * d);
+
+alert(10 / 0);
+alert(-10 / 0);
+
+let a = '5' + 2;
+alert(a);
+
+let a = 5 + '2';
+alert(a);
+
+let a = '5' * '2';
+alert(a);
+
+let a = '5' - '2';
+alert(a);
+
+let a = '5' / '2';
+alert(a);
+
+let a = '5' % '2';
+alert(a);
+
+let a = '5s' * '2';
+alert(a);
+
+let a = '5s' + '2';
+alert(a);
+
+let a = (-'5') + (-'2');
+alert(a);
+
+let a = '5' * 1 + '2' * 1;
+alert(a);
+
+let a = '5' * '1' + '2' * '1';
+alert(a);
+
+let a = '' + 3 + 1;
+alert(a);
+
+//Принудительное преобразование типов данных в JavaScript
+
+let a = '10';
+let b = '20';
+alert(Number(a) + Number(b));
+
+alert('2' + Number('3'));
+
+
+let a = +'2';
+let b = +3;
+alert(a + b);
+
+let q = '5px';
+let w = '6px';
+alert(parseFloat(q) + parseFloat(w));
+
+
+let q = '5.5px';
+let w = '6.25px';
+alert(parseFloat(q) + parseFloat(w) + 'px');
+
+let str = String(1234).length;
+alert(str);
+
+let num1 = 43456;
+let num2 = 34567655;
+alert(String(num1).length + String(num2).length);
+
+alert('1' + true);
+
+
+alert(String(true) + 1);
+*/
+alert(String(true) + Number(true));
+
+//Преобразование к логическому типу в JavaScript
