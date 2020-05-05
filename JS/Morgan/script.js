9ee9e5939ffe689088674071a0056ec1aa961d35
Oleg <oleg.bachinskiy@gmail.com>
2020-05-05 15:03:08 +0300

    '63'

--- JS/Morgan/script.js
+++ JS/Morgan/script.js
@@ -1,4 +1,5 @@
-"use strict"
+"use strict";
+/*
 let drawCats = function (howManyTimes) {
     for (let i = 0; i < howManyTimes; i++) {
         console.log(i + " =^.^=");
@@ -7,3 +8,39 @@ let drawCats = function (howManyTimes) {
 drawCats(4);
 
 //стр. 40
+//стр. 47
+let age = 11;
+let acco = true;
+if (age >= 11 || acco === true) {
+    console.log("можно");
+} else {
+    console.log("нельзя");
+}
+
+
+let dinosaurs = ["Тираннозавр", "Велоцираптор", "Стегозавр", "Трицератопс", "Брахиозавр", "Птеранодон", "Апатозавр", "Диплодок", "Компсогнат"];
+
+
+
+console.log(dinosaurs);
+
+var animals1 = ['Кабан', 'Рыба', 'Динозавр', '10т', 110];
+var animals2 = ['Крокодил', 'Кенгуру', 'Бегемот', 'Собака'];
+let animals3 = ['Ara', 'Dodo'];
+var newArray = animals1.concat(animals2, animals3);
+console.log(newArray.indexOf('Крокодил'));
+console.log(newArray.indexOf('Dodo'));
+console.log(newArray[2]);
+
+
+var animals1 = ['Кабан', 'Рыба', 'Динозавр', '10т', 110];
+var j = animals1.join(' =^-^= ');
+console.log(j);
+
+*/
+
+var animals1 = ['Кабан', 'Рыба', 'Динозавр', '10т', 110];
+var j = animals1.join(' и ');
+console.log(j);
+
+//63
