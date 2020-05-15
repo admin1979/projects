9786cf7e58590e0c4738b4e67764196a09c4fc13
Oleg <oleg.bachinskiy@gmail.com>
2020-05-15 12:01:19 +0300

    node begin

---
+++ JS/codemu/Node begin/index.js
@@ -0,0 +1,52 @@
+'use strict'
+// let a = 3;
+// let b = 4;
+// let c = 10;
+// console.log(a + b / 3 * c);
+
+// function func(a) {
+//     return a * a;
+// }
+
+// console.log(func(4));
+
+// let date = new Date();
+// console.log(date.getFullYear());
+
+// setInterval(function () {
+//     console.log('Hello World!!!');
+// }, 6000);
+
+// let i = 0;
+// setInterval(function () {
+//     console.log(i++);
+// }, 1000);
+
+// Первый урок решить задачи
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// setInterval(function () {
+//     console.log('!');
+// }, 1000);
+
+// setInterval(function () {
+//     let date = new Date();
+
+//     console.log(date.getMonth() + date.getDate());
+
+//     console.log(' ');
+//     console.log(date.getHours()); // часы
+//     console.log('часов');
+//     console.log(date.getMinutes()); // минуты
+//     console.log('минут');
+//     console.log(date.getSeconds()); // секунды
+//     console.log('секунд');
+// }, 1000);
+
+let i = 1;
+setInterval(function () {
+    console.log(i++);
+}, 1000);
