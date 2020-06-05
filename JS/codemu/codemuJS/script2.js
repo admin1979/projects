0aba5b428d5e3f5e2e31066e37ecaef91cc7820e
Oleg <oleg.bachinskiy@gmail.com>
2020-06-05 15:06:36 +0300

    67.Заполнение многомерных массивов JavaScript

--- JS/codemu/codemuJS/script2.js
+++ JS/codemu/codemuJS/script2.js
@@ -1775,6 +1775,86 @@ for (let i = 1; i <= 9; i++) {
   document.write('<br>');
 }
 
-*/
 
 // 65. Многомерные массивы в JavaScript
+
+let arr = [
+  ['a', 'b', 'c'],
+  ['d', 'e', 'f'],
+  ['g', 'h', 'i']
+];
+// console.log(arr[0][1]);
+console.log(arr[2][2]);
+
+let arr = [
+  ['a', 'b', 'c'],
+  ['d', 'e', 'f'],
+  ['g', 'h', 'i'],
+  ['j', 'k', 'l'],
+];
+console.log(arr[3][2], arr[1][1], arr[2][0], arr[0][0]);
+
+let arr = [
+  [1, 2],
+  [3, 4],
+  [5, 6]
+];
+let sum = arr[0][0] + arr[0][1] + arr[1][0] + arr[1][1] + arr[2][0] + arr[2][1];
+console.log(sum);
+
+
+let arr = [
+  [
+    [1, 2],
+    [3, 4],
+  ],
+  [
+    [5, 6],
+    [7, 8],
+  ],
+];
+
+let sum = arr[0][0][0] + arr[0][1][1] + arr[0][1][0] + arr[0][1][1] + arr[1][0][0] + arr[1][1][1] + arr[1][1][0] + arr[1][1][1];
+console.log(sum); //ошибка где-то, пошли дальше, принцип понятен
+
+
+// 66. Перебор многомерных массивов в JavaScript
+
+let arr = [
+  [1, 2, 3, 4, 5],
+  [6, 7, 8],
+  [9, 10]
+];
+
+for (let subArr of arr) {
+  for (let elem of subArr) {
+    console.log(elem);
+  }
+}
+
+let arr = [
+  [1, 2, 3],
+  [4, 5],
+  [6]
+];
+for (let subArr of arr) {
+  for (let elem of subArr) {
+    console.log(elem + elem);
+  }
+}
+
+
+let arr = [
+  [1, 2, 3, 4, 5],
+  [6, 7, 8],
+  [9, 10]
+];
+
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < arr[i].length; j++) {
+    console.log(arr[i][j]);
+  }
+}
+*/
+
+// 67.Заполнение многомерных массивов JavaScript
