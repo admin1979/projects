e81f40529b607b0df54bfbc1935b4a6c245e99d0
Oleg <oleg.bachinskiy@gmail.com>
2020-07-10 17:05:52 +0300

    195.Обращение через методы к data атрибутам в JavaScript

--- JS/codemu/codemuJS/script7.js
+++ JS/codemu/codemuJS/script7.js
@@ -1,2 +1,37 @@
+// 193.Пользовательские атрибуты
+
+/* let elem = document.querySelector('#elem');
+alert(elem.dataset.num); // выведет 1000 
+
+let elem1 = document.querySelector('#elem1');
+
+elem1.addEventListener('click', function () {
+    elem1.innerHTML += elem1.dataset.text;
+});
+
+let div = document.querySelectorAll('div');
+
+
+for (let elem of div) {
+    elem.addEventListener('click', function () {
+        elem.innerHTML += elem.dataset.num;
+    });
+}
+
+
+// 194.Пользовательские атрибуты с дефисами в JavaScript
+
 let elem = document.querySelector('#elem');
-alert(elem.dataset.num); // выведет 1000
+
+elem.addEventListener('click', function () {
+    elem.innerHTML += '=' + elem.dataset.productPrice * elem.dataset.productAmount + 'руб';
+});
+
+*/
+
+// 195.Обращение через методы к data атрибутам в JavaScript
+
+let elem = document.querySelector('#elem');
+
+alert(elem.getAttribute('data-num')); // выведет 1000
+alert(elem.getAttribute('data-my-num')); // выведет 2000
