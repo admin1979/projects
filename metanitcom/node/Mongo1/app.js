cfb4bc045e8fc3b86718ac97bf0438ac7dad60b5
admin1979 <oleg bachinskiy@gmail.com>
2020-09-29 15:27:03 +0300

    29 сентября

--- metanitcom/node/Mongo1/app.js
+++ metanitcom/node/Mongo1/app.js
@@ -1,20 +1,20 @@
 const mysql = require("mysql2");
-  
+
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   database: "usersdb2",
-  password: "1z8UyM75Q54z17"
+  password: "пароль надададададад"
 });
- 
+
 const user = ["Tom", 29];
 const sql = "INSERT INTO users(name, age) VALUES(?, ?)";
- 
-connection.query(sql, user, function(err, results) {
-    if(err) console.log(err);
-    else console.log("Данные добавлены");
+
+connection.query(sql, user, function (err, results) {
+  if (err) console.log(err);
+  else console.log("Данные добавлены");
 });
- 
+
 connection.end();
 
 // Глава 7. ===> Выполнение запросов к MySQL
