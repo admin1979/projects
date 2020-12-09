84de708ad138ae427bcdc85ebc205047042a608a
Oleg <oleg.bachinskiy@gmail.com>
2020-12-09 12:05:19 +0300

    C# SC 35

---
+++ dotnet C#/SimpleCode/SimpleCode35/35.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _35._1
+{
+    class Program
+    {
+        static void PrintLine()
+        {
+            Console.WriteLine("Метод PrintLine был вызван!");
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            PrintLine();
+        }
+    }
+}
---
+++ dotnet C#/SimpleCode/SimpleCode35/SimpleCode35/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCode35
+{
+    class Program
+    {
+        static int Sum(int a, int b)
+        {
+            int result = a + b;
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+            c = Sum(a, b);
+
+
+            Console.WriteLine(c);
+
+            Console.ReadKey();
+
+        }
+    }
+}
