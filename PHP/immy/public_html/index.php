a423d2fbe8c7b818947f81b25f54d238dcfc9c8e
Oleg <oleg.bachinskiy@gmail.com>
2021-01-16 13:09:00 +0300

    добавил комментарии

--- PHP/immy/public_html/index.php
+++ PHP/immy/public_html/index.php
@@ -1,9 +1,9 @@
 <?php
 
-define('VG_ACCESS', true); //разрешение (true) доступа к config левым людям это ведь index
+define('VG_ACCESS', true); //(Константа безопасности)разрешение (true) доступа к config левым людям это ведь index
 
-header('Content-Type:text/html;charset=utf-8');
-session_start();
+header('Content-Type:text/html;charset=utf-8'); //отправка браузеру пользователя заголовка с кодировкой
+session_start(); //старт сессии?????
 
 require_once 'config.php'; //Путь к настройкам хостинга 
 require_once 'core/base/internal_settings.php'; //Путь к настройкам проекта
