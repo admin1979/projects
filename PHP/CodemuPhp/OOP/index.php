5a5a88624083ca2e3e315501461aceefca564406
Oleg <oleg.bachinskiy@gmail.com>
2021-02-26 11:25:03 +0300

    след Конструктор объекта

--- PHP/CodemuPhp/OOP/index.php
+++ PHP/CodemuPhp/OOP/index.php
@@ -1,22 +1,54 @@
 <?php
-class User
+class Employee
 {
     public $name;
     public $age;
+    public $salary;
 
-    // Создаем метод:
-    public function show($str)
+    public function show()
     {
-        return $str . '!!!';
+        return $this ->name;
     }
+
+    public function show2()
+    {
+        return $this ->age;
+    }
+
+    public function show3()
+    {
+        return $this ->salary;
+    }
+
+    public function checkAge()
+    {
+        if ($this->age > 18)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
 }
 
-$user = new User;
-$user->name = 'Коля';
-$user->age = 25;
+$Empl = new Employee;
+$Empl ->name = 'Вася';
+$Empl ->age = 19;
+$Empl ->salary = 100500;
 
-// Вызовем наш метод:
-echo $user->show('hello'); // выведет '!!!'
-echo $user->name;
-?>
+
+echo $Empl ->name;
+echo "\r";
+echo $Empl ->show();
+echo "\r";
+echo $Empl ->show2();
+echo "\r";
+echo $Empl ->show3();
+echo "\r";
+echo $Empl ->checkAge();
+echo "\r";
+
 
